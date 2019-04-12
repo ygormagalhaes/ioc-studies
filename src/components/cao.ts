13f2b8ef9a15755df5ae4604bf77f7cbc6086730
Ygor <emaildoygor@gmail.com>
2019-04-12 17:29:47 -0300

    Adição de comentário a respeito da forma como o conceito de ioc foi implementado

--- src/components/cao.ts
+++ src/components/cao.ts
@@ -2,6 +2,11 @@ import Container from "../container/container";
 import IRacao from "../interfaces/racao";
 
 export default class Cao {
+    /**
+     * FIXME: Verificar se essa forma de obtenção do 'componente' não se comporta como um Service Locator.
+     * Já que o desenvolvedor precisa pedir explicitamente o tipo de dado que deseja, contrariando assim o conceito
+     * de inversão de controle.
+     */
     private racao: IRacao = Container.get<IRacao>("Racao", "Cao");
     private peso: number = 3;
 

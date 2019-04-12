fcb5d139a200b1ea9770b21365b2f42f207bce8e
Ygor <emaildoygor@gmail.com>
2019-04-12 17:33:01 -0300

    Complementando comentário com trecho de artigo do Fowler

--- src/components/cao.ts
+++ src/components/cao.ts
@@ -6,6 +6,10 @@ export default class Cao {
      * FIXME: Verificar se essa forma de obtenção do 'componente' não se comporta como um Service Locator.
      * Já que o desenvolvedor precisa pedir explicitamente o tipo de dado que deseja, contrariando assim o conceito
      * de inversão de controle.
+     * "The important difference between the two patterns is about how that implementation is provided
+     * to the application class. With service locator the application class asks for it explicitly by
+     * a message to the locator. With injection there is no explicit request, the service appears in
+     * the application class - hence the inversion of control." - Martin Fowler
      */
     private racao: IRacao = Container.get<IRacao>("Racao", "Cao");
     private peso: number = 3;
